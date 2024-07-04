OlivierNicole/js_of_ocaml

Optimize sourcemap processing

compiler/lib/source_map_io.yojson.ml
@@ -19,47 +19,57 @@
 
 open Source_map
 
+let rewrite_path path =
+  if Filename.is_relative path
+  then path
+  else
+    match Build_path_prefix_map.get_build_path_prefix_map () with
+    | Some map -> Build_path_prefix_map.rewrite map path
+    | None -> path
+
+(* Escapes special characters and wrap in double quotes *)
+let stringlit_of_string s = `Stringlit (Yojson.Basic.to_string (`String s))
+
 let json t =
-  let rewrite_path path =
-    if Filename.is_relative path
-    then path
-    else
-      match Build_path_prefix_map.get_build_path_prefix_map () with
-      | Some map -> Build_path_prefix_map.rewrite map path
-      | None -> path
-  in
-  `Assoc
-    [ "version", `Float (float_of_int t.version)
-    ; "file", `String (rewrite_path t.file)
+  let (Source_map.Mappings.Uninterpreted mappings) = t.mappings in
+  let fields =
+    [ "version", `Intlit (Int.to_string t.version)
+    ; "file", stringlit_of_string (rewrite_path t.file)
     ; ( "sourceRoot"
-      , `String
+      , stringlit_of_string
           (match t.sourceroot with
           | None -> ""
           | Some s -> rewrite_path s) )
-    ; "names", `List (List.map (fun s -> `String s) t.names)
-    ; "sources", `List (List.map (fun s -> `String (rewrite_path s)) t.sources)
-    ; "mappings", `String (string_of_mapping t.mappings)
-    ; ( "sourcesContent"
-      , `List
-          (match t.sources_content with
-          | None -> []
-          | Some l ->
-              List.map
-                (function
-                  | None -> `Null
-                  | Some s -> `String s)
-                l) )
+    ; "names", `List (List.map (fun s -> stringlit_of_string s) t.names)
+    ; ( "sources"
+      , `List (List.map (fun s -> stringlit_of_string (rewrite_path s)) t.sources) )
+    ; "mappings", `Stringlit ("\"" ^ mappings ^ "\"") (* Nothing to escape *)
     ]
+  in
+  match t.sources_contents with
+  | None -> `Assoc fields
+  | Some (Source_map.Sources_contents.Uninterpreted cs) ->
+      `Assoc
+        (fields
+        @ [ ( "sourcesContent"
+              (* It is the job of {!mod:Sources_contents} to enforce that [cs] is
+                 already a valid JSON string literal *)
+            , `Stringlit cs )
+          ])
 
 let invalid () = invalid_arg "Source_map.of_json"
 
+let string_of_stringlit (`Stringlit s) =
+  match Yojson.Basic.from_string s with
+  | `String s -> s
+  | _ -> invalid_arg "Source_map_io.string_of_stringlit: not a JSON string literal"
+
 let string name rest =
-  try
-    match List.assoc name rest with
-    | `String s -> Some s
-    | `Null -> None
-    | _ -> invalid ()
-  with Not_found -> None
+  match List.assoc name rest with
+  | `Stringlit _ as s -> Some (string_of_stringlit s)
+  | `Null -> None
+  | _ -> invalid ()
+  | exception Not_found -> None
 
 let list_string name rest =
   try
@@ -68,54 +78,81 @@ let list_string name rest =
         Some
           (List.map
              (function
-               | `String s -> s
+               | `Stringlit _ as lit -> string_of_stringlit lit
                | _ -> invalid ())
              l)
     | _ -> invalid ()
   with Not_found -> None
 
-let list_string_opt name rest =
-  try
-    match List.assoc name rest with
-    | `List l ->
-        Some
-          (List.map
-             (function
-               | `String s -> Some s
-               | `Null -> None
-               | _ -> invalid ())
-             l)
-    | _ -> invalid ()
-  with Not_found -> None
+let stringlit_opt name assoc =
+  match List.assoc name assoc with
+  | `Stringlit s -> Some s
+  | _ | (exception Not_found) -> None
 
 let of_json json =
   match json with
-  | `Assoc (("version", `Float version) :: rest) when int_of_float version = 3 ->
-      let def v d =
-        match v with
-        | None -> d
-        | Some v -> v
-      in
+  | `Assoc (("version", version) :: rest) ->
+      (match version with
+      | `Floatlit version when Float.equal (Float.of_string version) 3.0 -> ()
+      | `Intlit version when Int.equal (int_of_string version) 3 -> ()
+      | `Floatlit _ | `Intlit _ -> invalid_arg "Source_map_io.of_json: version != 3"
+      | _ -> invalid_arg "Source_map_io.of_json: version field is not a number");
       let file = string "file" rest in
       let sourceroot = string "sourceRoot" rest in
       let names = list_string "names" rest in
       let sources = list_string "sources" rest in
-      let sources_content = list_string_opt "sourcesContent" rest in
-      let mappings = string "mappings" rest in
-      { version = int_of_float version
-      ; file = def file ""
+      let sources_contents = stringlit_opt "sourcesContent" rest in
+      let mappings = stringlit_opt "mappings" rest in
+      let mappings =
+        Option.map
+          (fun mappings ->
+            assert (
+              String.length mappings > 2
+              && Char.equal mappings.[0] '"'
+              && Char.equal mappings.[String.length mappings - 1] '"');
+            let mappings = String.sub mappings 1 (String.length mappings - 2) in
+            Mappings.uninterpreted mappings)
+          mappings
+      in
+      { version = 3
+      ; file = Option.value file ~default:""
       ; sourceroot
-      ; names = def names []
-      ; sources_content
-      ; sources = def sources []
-      ; mappings = mapping_of_string (def mappings "")
+      ; names = Option.value names ~default:[]
+      ; sources_contents = Option.map Sources_contents.uninterpreted sources_contents
+      ; sources = Option.value sources ~default:[]
+      ; mappings = Option.value mappings ~default:Mappings.empty
       }
   | _ -> invalid ()
 
-let of_string s = of_json (Yojson.Basic.from_string s)
+let of_string s = of_json (Yojson.Raw.from_string s)
 
-let to_string m = Yojson.Basic.to_string (json m)
+let to_string m = Yojson.Raw.to_string (json m)
 
-let to_file m file = Yojson.Basic.to_file file (json m)
+let to_file m file = Yojson.Raw.to_file file (json m)
 
 let enabled = true
+
+module Composite = struct
+  let json t =
+    `Assoc
+      [ "version", `Intlit (Int.to_string t.Composite.version)
+      ; "file", stringlit_of_string (rewrite_path t.file)
+      ; ( "sections"
+        , `List
+            (List.map
+               (fun ({ Composite.gen_line; gen_column }, `Map sm) ->
+                 `Assoc
+                   [ ( "offset"
+                     , `Assoc
+                         [ "line", `Intlit (Int.to_string gen_line)
+                         ; "column", `Intlit (Int.to_string gen_column)
+                         ] )
+                   ; "map", json sm
+                   ])
+               t.sections) )
+      ]
+
+  let to_string m = Yojson.Raw.to_string (json m)
+
+  let to_file m file = Yojson.Raw.to_file file (json m)
+end

OlivierNicole/wasm_of_ocaml

compiler/lib/source_map_io.yojson.ml
@@ -19,48 +19,58 @@
 
 open Source_map
 
-let json ?replace_mappings t =
-  let rewrite_path path =
-    if Filename.is_relative path
-    then path
-    else
-      match Build_path_prefix_map.get_build_path_prefix_map () with
-      | Some map -> Build_path_prefix_map.rewrite map path
-      | None -> path
+let rewrite_path path =
+  if Filename.is_relative path
+  then path
+  else
+    match Build_path_prefix_map.get_build_path_prefix_map () with
+    | Some map -> Build_path_prefix_map.rewrite map path
+    | None -> path
+
+(* Escapes special characters and wrap in double quotes *)
+let stringlit_of_string s = `Stringlit (Yojson.Basic.to_string (`String s))
+
+let json t =
+  let mappings =
+    `Stringlit ("\"" ^ Mappings.to_string t.mappings ^ "\"")
+    (* Nothing to escape *)
   in
-  `Assoc
-    [ "version", `Float (float_of_int t.version)
-    ; "file", `String (rewrite_path t.file)
+  let fields =
+    [ "version", `Intlit (Int.to_string t.version)
+    ; "file", stringlit_of_string (rewrite_path t.file)
     ; ( "sourceRoot"
-      , `String
+      , stringlit_of_string
           (match t.sourceroot with
           | None -> ""
           | Some s -> rewrite_path s) )
-    ; "names", `List (List.map (fun s -> `String s) t.names)
-    ; "sources", `List (List.map (fun s -> `String (rewrite_path s)) t.sources)
-    ; ( "mappings"
-      , `String (Option.value ~default:(string_of_mapping t.mappings) replace_mappings) )
-    ; ( "sourcesContent"
-      , `List
-          (match t.sources_content with
-          | None -> []
-          | Some l ->
-              List.map
-                (function
-                  | None -> `Null
-                  | Some s -> `String s)
-                l) )
+    ; "names", `List (List.map (fun s -> stringlit_of_string s) t.names)
+    ; ( "sources"
+      , `List (List.map (fun s -> stringlit_of_string (rewrite_path s)) t.sources) )
+    ; "mappings", mappings
     ]
+  in
+  match t.sources_contents with
+  | None -> `Assoc fields
+  | Some cs ->
+      `Assoc
+        (fields
+        @ [ ( "sourcesContent"
+            , `List (List.map (fun t -> `Stringlit (Source_text.to_json_string t)) cs) )
+          ])
 
 let invalid () = invalid_arg "Source_map_io.of_json"
 
+let string_of_stringlit (`Stringlit s) =
+  match Yojson.Basic.from_string s with
+  | `String s -> s
+  | _ -> invalid_arg "Source_map_io.string_of_stringlit: not a JSON string literal"
+
 let string name rest =
-  try
-    match List.assoc name rest with
-    | `String s -> Some s
-    | `Null -> None
-    | _ -> invalid ()
-  with Not_found -> None
+  match List.assoc name rest with
+  | `Stringlit _ as s -> Some (string_of_stringlit s)
+  | `Null -> None
+  | _ -> invalid ()
+  | exception Not_found -> None
 
 let list_string name rest =
   try
@@ -69,46 +79,64 @@ let list_string name rest =
         Some
           (List.map
              (function
-               | `String s -> s
+               | `Stringlit _ as lit -> string_of_stringlit lit
                | _ -> invalid ())
              l)
     | _ -> invalid ()
   with Not_found -> None
 
-let list_string_opt name rest =
-  try
-    match List.assoc name rest with
-    | `List l ->
-        Some
-          (List.map
-             (function
-               | `String s -> Some s
-               | `Null -> None
-               | _ -> invalid ())
-             l)
-    | _ -> invalid ()
-  with Not_found -> None
+let stringlit_opt name assoc =
+  match List.assoc name assoc with
+  | `Stringlit s -> Some s
+  | _ | (exception Not_found) -> None
 
-let of_json ~parse_mappings json =
-  let parse ~version rest =
-    let def v d =
-      match v with
-      | None -> d
-      | Some v -> v
-    in
-    let file = string "file" rest in
-    let sourceroot = string "sourceRoot" rest in
-    let names = list_string "names" rest in
-    let sources = list_string "sources" rest in
-    let sources_content = list_string_opt "sourcesContent" rest in
-    let mappings = string "mappings" rest in
-    ( { version
-      ; file = def file ""
+let stringlit_list_opt name assoc =
+  match List.assoc name assoc with
+  | `List l ->
+      Some
+        (List.map
+           (function
+             | `Stringlit lit -> lit
+             | _ -> invalid ())
+           l)
+  | _ -> invalid ()
+  | exception Not_found -> None
+
+let standard_map_of_json json =
+  match json with
+  | `Assoc (("version", version) :: rest) ->
+      (match version with
+      | `Floatlit version when Float.equal (Float.of_string version) 3.0 -> ()
+      | `Intlit version when Int.equal (int_of_string version) 3 -> ()
+      | `Floatlit _ | `Intlit _ -> invalid_arg "Source_map_io.of_json: version != 3"
+      | _ -> invalid_arg "Source_map_io.of_json: version field is not a number");
+      (match List.assoc "sections" rest with
+      | _ -> invalid_arg "Source_map_io.standard_map_of_json: not a standard map"
+      | exception Not_found -> ());
+      let file = string "file" rest in
+      let sourceroot = string "sourceRoot" rest in
+      let names = list_string "names" rest in
+      let sources = list_string "sources" rest in
+      let sources_contents = stringlit_list_opt "sourcesContent" rest in
+      let mappings = stringlit_opt "mappings" rest in
+      let mappings =
+        Option.map
+          (fun mappings ->
+            assert (
+              String.length mappings >= 2
+              && Char.equal mappings.[0] '"'
+              && Char.equal mappings.[String.length mappings - 1] '"');
+            Mappings.of_string (String.sub mappings 1 (String.length mappings - 2)))
+          mappings
+      in
+      { version = 3
+      ; file = Option.value file ~default:""
       ; sourceroot
-      ; names = def names []
-      ; sources_content
-      ; sources = def sources []
-      ; mappings = (if parse_mappings then mapping_of_string (def mappings "") else [])
+      ; names = Option.value names ~default:[]
+      ; sources_contents =
+          Option.map (List.map Source_text.of_json_string) sources_contents
+      ; sources = Option.value sources ~default:[]
+      ; mappings = Option.value mappings ~default:Mappings.empty
       }
     , if parse_mappings then None else mappings )
   in
@@ -118,15 +146,100 @@ let of_json ~parse_mappings json =
   | `Assoc (("version", `Int 3) :: rest) -> parse ~version:3 rest
   | _ -> invalid ()
 
-let of_string s = of_json ~parse_mappings:true (Yojson.Basic.from_string s) |> fst
+let to_string m = Yojson.Raw.to_string (json m)
 
-let to_string m = Yojson.Basic.to_string (json m)
-
-let to_file ?mappings m ~file =
-  let replace_mappings = mappings in
-  Yojson.Basic.to_file file (json ?replace_mappings m)
-
-let of_file_no_mappings filename =
-  of_json ~parse_mappings:false (Yojson.Basic.from_file filename)
+let to_file m file = Yojson.Raw.to_file file (json m)
 
 let enabled = true
+
+module Index = struct
+  let json t =
+    `Assoc
+      [ "version", `Intlit (Int.to_string t.Index.version)
+      ; "file", stringlit_of_string (rewrite_path t.file)
+      ; ( "sections"
+        , `List
+            (List.map
+               (fun ({ Index.gen_line; gen_column }, `Map sm) ->
+                 `Assoc
+                   [ ( "offset"
+                     , `Assoc
+                         [ "line", `Intlit (Int.to_string gen_line)
+                         ; "column", `Intlit (Int.to_string gen_column)
+                         ] )
+                   ; "map", json sm
+                   ])
+               t.sections) )
+      ]
+
+  let intlit ~errmsg name json =
+    match List.assoc name json with
+    | `Intlit i -> int_of_string i
+    | _ -> invalid_arg errmsg
+    | exception Not_found -> invalid_arg errmsg
+
+  let section_of_json : Yojson.Raw.t -> Index.offset * [ `Map of t ] = function
+    | `Assoc json ->
+        let offset =
+          match List.assoc "offset" json with
+          | `Assoc fields ->
+              let gen_line =
+                intlit
+                  "line"
+                  fields
+                  ~errmsg:
+                    "Source_map_io.Index.of_json: field 'line' absent or invalid from \
+                     section"
+              in
+              let gen_column =
+                intlit
+                  "column"
+                  fields
+                  ~errmsg:
+                    "Source_map_io.Index.of_json: field 'column' absent or invalid from \
+                     section"
+              in
+              Index.{ gen_line; gen_column }
+          | _ ->
+              invalid_arg "Source_map_io.Index.of_json: 'offset' field of unexpected type"
+        in
+        (match List.assoc "url" json with
+        | _ ->
+            invalid_arg
+              "Source_map_io.Index.of_json: URLs in index maps are not currently \
+               supported"
+        | exception Not_found -> ());
+        let map =
+          try standard_map_of_json (List.assoc "map" json) with
+          | Not_found -> invalid_arg "Source_map_io.Index.of_json: field 'map' absent"
+          | Invalid_argument _ ->
+              invalid_arg "Source_map_io.Index.of_json: invalid sub-map object"
+        in
+        offset, `Map map
+    | _ -> invalid_arg "Source_map_io.Index.of_json: section of unexpected type"
+
+  let of_json = function
+    | `Assoc fields -> (
+        let file = string "file" fields in
+        match List.assoc "sections" fields with
+        | `List sections ->
+            let sections = List.map section_of_json sections in
+            { Index.version = 3; file = Option.value file ~default:""; sections }
+        | _ -> invalid_arg "Source_map_io.Index.of_json: `sections` is not an array"
+        | exception Not_found ->
+            invalid_arg "Source_map_io.Index.of_json: no `sections` field")
+    | _ -> invalid_arg "Source_map_io.of_json: map is not an object"
+
+  let to_string m = Yojson.Raw.to_string (json m)
+
+  let to_file m file = Yojson.Raw.to_file file (json m)
+end
+
+let of_json = function
+  | `Assoc fields as json -> (
+      match List.assoc "sections" fields with
+      | _ -> `Index (Index.of_json json)
+      | exception Not_found -> `Standard (standard_map_of_json json))
+  | _ -> invalid_arg "Source_map_io.of_json: map is not an object"
+
+let of_string s = of_json (Yojson.Raw.from_string s)